0c2a2790446e9f9184253055cf3069e461b9a466
ato lash <28973861+hal-shu-sato@users.noreply.github.com>
2024-07-28 22:47:30 +0900

    ignore dist files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
 
 import eslint from "@eslint/js";
-import tseslint from "typescript-eslint";
 import eslintConfigPrettier from "eslint-config-prettier";
+import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    ignores: ["dist"],
+  },
   eslintConfigPrettier,
 );
