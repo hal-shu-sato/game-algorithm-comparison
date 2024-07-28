1021446da131e842cb95c0a8013d3d5d4bc9c8e7
ato lash <28973861+hal-shu-sato@users.noreply.github.com>
2024-07-28 21:51:45 +0900

    adjust constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 export const BOARD_SIZE = 9;
-export const MAX_DEPTH = 9;
+export const MAX_DEPTH = Infinity;
 export const HASH_SIZE = 1024;
-export const STACK_SIZE = 1024;
-export const QUEUE_SIZE = 1024;
-export const HEAP_SIZE = 1024;
+export const STACK_SIZE = 1024 * 256;
+export const QUEUE_SIZE = 1024 * 256;
+export const HEAP_SIZE = 1024 * 64;
